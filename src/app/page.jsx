47f68bd50f4e0a733934a99439459f86ba4c8cd6
Vitor Dallanol <125999178+Crautor@users.noreply.github.com>
2024-06-18 10:14:58 -0300

    feat: adicionando recursos base para a pagina

---
+++ src/app/page.jsx
@@ -0,0 +1,14 @@
+'use client';
+import React from 'react';
+
+import Content from '@/components/Content';
+
+const page = () => {
+  return (
+    <div className=''>
+      <Content />
+    </div>
+  );
+};
+
+export default page;
